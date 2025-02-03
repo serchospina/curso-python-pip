e717fe17dddfb577491699dc5c5d22b6dd0e0130
daniospinaubun <ingedanielospina9@gmail.com>
2025-02-03 12:47:01 -0500

    Agregamos requirement.txt y modificamos instrucciones en el README.md

---
+++ app/main.py
@@ -0,0 +1,27 @@
+import utils
+import read_csv
+import charts
+
+
+def run():
+  data = read_csv.read_csv('data.csv')
+  data = list(filter(lambda item: item['Continent'] == 'South America', data))
+
+  countries = list(map(lambda x: x['Country/Territory'], data))
+  porcentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, porcentages)
+  
+  country = input('Type Country => ')
+  print(country)
+
+  result = utils.population_by_country(data, country)
+
+  if len(result) > 0:
+    country = result[0]
+    print(country)
+    label, values = utils.get_population(country)
+    charts.generate_bar_chart(country['Country/Territory'], label, values)
+  
+
+if __name__ == '__main__':
+  run()
